b1fecea82b8639c6dd1342748e9e1253092d99a4
Santosh <santosh@Santoshs-MacBook-Air.local>
2025-11-04 21:12:35 +0530

    fixed login

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-import app from '../src/app.js';
-import { connectDB } from '../src/config/database.js';
+import app from '../src/app';
+import { connectDB } from '../src/config/database';
 import { Request, Response } from 'express';
 
 // Track connection state for serverless optimization
